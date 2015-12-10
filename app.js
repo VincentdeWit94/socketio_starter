1bcedab20031240201b87598d8dcadc927ebc7d4
Vincent de Wit <info@appsbyvincent.nl>
2015-12-10 06:56:47 +0100

    Switched to ws server

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
-var app = require('express')();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
+"use strict";
+/* ************************************************************************
+ 
+    Licence : LGPLv3
+    Version: 0.1
+    Authors: Vincent de Wit
+    Date: 2015-12-09
+    Date of last modification: 2015-12-09
+    Description: Digital Entrance Systems Server
+ 
+************************************************************************ */
 
-server.listen(80);
+var ws = require("nodejs-websocket");
 
-app.get('/', function (req, res) {
-
-});
-
-io.on('connection', function (socket) {
-  socket.emit('alive', { hello: 'world' });
-  socket.on('alive', function (data) {
-    console.log(data);
-  });
-});
+var server = ws.createServer(function (conn) {
+	console.log("New connection")
+	conn.on("text", function (str) {
+		console.log("Received "+str)
+		conn.sendText(str.toUpperCase()+"!!!")
+	})
+	conn.on("close", function (code, reason) {
+		console.log("Connection closed")
+	})
+}).listen(8001)
