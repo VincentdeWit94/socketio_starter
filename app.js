cc65d78ca7e9ffa4658cf5568a69eec286516024
VincentdeWit94 <vincent.dewit@student.hu.nl>
2016-03-14 14:07:03 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-/*
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
-// var io = require('../..')(server);
-// New:
-var io = require('socket.io')(server);
-*/
 var port = process.env.PORT || 3000;
 
 var app = require('express')()
